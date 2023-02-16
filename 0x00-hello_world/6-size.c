44fc5b4127485582e8e0daa304d6fbaaa7386081
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-16 05:49:01 -0800

    change

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,4 +19,5 @@ int main(void)
 				printf("size of a long long int: %lu byte(s)\n" (unsigned long)sizeof(d));
 				printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 				return (0);
-				}
+				
+			}
