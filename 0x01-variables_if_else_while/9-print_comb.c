0b0420b4006f5e8df8cf043ff7f6f9528179c9eb
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 13:31:58 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combinations of numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i +  '0');
+			if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
