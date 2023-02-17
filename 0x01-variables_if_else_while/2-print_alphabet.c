8f45ca15750dfec31955e8f34ce2e1be0d0080a8
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 08:43:06 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		putchar('\n');
+	return (0);
+}
