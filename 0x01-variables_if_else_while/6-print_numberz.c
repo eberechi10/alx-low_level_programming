49623f6f74e98514916b02969dcf4936181deca7
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 11:47:22 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print all numbers of base 10, starting from 0.
+ * you are not allowed to use any variiable of type char.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++")
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return(0);
+
+}
