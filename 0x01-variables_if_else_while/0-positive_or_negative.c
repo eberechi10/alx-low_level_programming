4a985f1055ae554fb22da8cc74b0dd412ddde60d
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 00:53:26 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -19,9 +20,9 @@ int main(void)
 		{
 			printf("%d is zero\n");
 		}
-	else 
+else
 	{
 		printf("%d is negative\n");
 	}
-	retune (0);
+	retune(0);
 }
