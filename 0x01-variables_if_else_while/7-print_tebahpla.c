efd16b5deef4b33510ca3ea9368de4719d95cf41
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 12:18:32 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in a reverse orde.
+ *
+ * Reture: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; C >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	puter('\n');
+
+	return (0);
+}
