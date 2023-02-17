ea39b1214d691ca41a1e591aef00c8ccb091af82
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 13:04:45 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - print hexdecima numbers in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
