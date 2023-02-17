d8a282e9d134ab83767a525db048724d3de753c5
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 09:56:32 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main = print all alphabets but 'q' and e.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
+
