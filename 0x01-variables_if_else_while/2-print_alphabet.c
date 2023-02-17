d26d71370472fb7b1c472cf49463473610a30bc4
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 09:17:47 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,13 @@
  */
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
 		putchar('\n');
 	return (0);
+
 }
