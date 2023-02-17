730f386444f9a3d1514a39fbc57f650155b54457
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 12:32:59 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - print alphabet in a reverse orde.
+ * main - print alphabet in a reverse order.
  *
- * Reture: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
