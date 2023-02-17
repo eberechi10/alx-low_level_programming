4b050a475b84dda746f1655ba1a3cf04e0581f42
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-17 04:42:26 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	}
 		else if (n == 0)
 		{
-			printf("%d is zero\n");
+			printf("%d is zero\n", n);
 		}
 else
 	{
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
 	}
 	retune(0);
 }
