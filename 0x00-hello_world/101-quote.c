04d4d14c7496ded78155ba9df8ad5a04fdb84e9e
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-18 10:33:55 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+
 #include <unistd.h>
 
 /**
@@ -7,7 +8,7 @@
  */
 int main(void)
 {
-	char str1[] = "and that peice of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, str1, 59);
 	return (1);
