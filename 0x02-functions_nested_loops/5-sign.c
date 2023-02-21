15a3e5e26ff3ddd660c732fafd65bedba08e9ce6
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-21 02:43:45 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of number.
+ * @n: The number of which sign to print
+ * Return: 1 if greater than zero, 0 if zero and -1 if less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
+
