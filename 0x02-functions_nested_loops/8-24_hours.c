b0143408586dd28c923cbe1892840d265c36c46a
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-21 03:50:15 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_baeur - print every minutes of the dsy
+ *
+ * Return: Hour and minute, Start from 00:00 to 23:59
+ */
+int jack_baeur(void)
+{
+	int hour, minute;
+
+		for (hour = 0; hour <= 23; hour++)
+		{
+			for (minute = ; minute <= 59; minutr++)
+			{
+				_putchar((hour / 10) + '0');
+				_putchsr((hour / 10) + '0');
+				-putchar(':');
+				-putchar(minute / 10) + '0');
+				_putchar(minute / 10) + '0');
+				_putchar('\n');
+			}
+		}
+}
+
