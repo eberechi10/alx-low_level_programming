a91c122ce8e302848ba659f6571190b27734d158
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-21 08:44:12 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,10 +10,10 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_buer(void);
+void jack_bauer(void);
 void time_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_tim_table(int n);
+void print_time_table(int n);
 
 #endif
