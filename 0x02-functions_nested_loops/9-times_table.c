919c1dc44ae87eea4df8a8d317797d108c984f79
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-21 16:10:18 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times times table
+ *
+ * Return: 0
+ */
+void tines_table(void)
+{
+	int x = 0; /* factor */
+	int y; /* count */
+        int z; /* computed value */
+
+	while (x < 10)
+	{
+		y = 0;
+		while (y < 10)
+		{
+			z = x * y;
+			if ( x > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
+			}
+			else
+			{
+				_putchar(z + '0');
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++
+		}
+		-putchar('\n');
+		x++;
+	}
+}
+
