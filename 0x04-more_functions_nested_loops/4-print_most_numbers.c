dd9610d04e4e54a71ab178f07db7a39c22c50c37
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-23 11:12:59 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 - 9 excluding 2 and 4.
- *
- * Return: you can use _putchar twice.*/
+*print_most_numbers - prints
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_most_numbers(void)
+{
+	char c;
 
-	int i;
-
-	i = 0;
-	while (i < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (i != 2 && i != 4)
-			_putchar(i + '0');
-
-		i++;
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
