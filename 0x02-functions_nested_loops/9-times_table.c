a10f8b9fc99b50b33194dc5f7afb23d87f295f13
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-22 23:38:11 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,36 +1,45 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * times_table - print the 9 times times table
- *
+ * times_table - Print the 9 times table, starting with 0
  * Return: 0
  */
 void times_table(void)
 {
-	int r, c, d;
+	int x = 0; /* factor */
+	int y; /* count  */
+	int z; /* computed value */
 
-	for (r = 0; r <= 9; r++)
+	while (x < 10)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (c = 1; c <= 9; c++)
+		y = 0;
+		while (y < 10)
 		{
-			d = (r * c);
-			if ((d / 10) > 0)
-	{
-				_putchar((d / 10) + '0');
+			z = x * y;
+
+			if (z > 9)
+			{
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			else if (y != 0)
+			{
+				_putchar(' ');
+				_putchar(z + '0');
 			}
 			else
 			{
-				_putchar(' ');
+				_putchar(z + '0');
 			}
-			if (c < 9)
+
+			if (y != 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
+			y++;
 		}
 		_putchar('\n');
+		x++;
 	}
 }
+
