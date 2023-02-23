4a23d47138301e75bfffbc255518b4e1397767ad
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-02-23 11:08:54 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
+
 /**
- * print_numbers - print number 0 -9.
- *
- * Return: You can only use _putchar twice.
- */
+*print_numbers - print 0123456789
+* owned by Bwave ICT
+*Return: void
+*/
+
 void print_numbers(void)
 {
-	int i
+	char c;
 
-		i = 0;
-	while (i < 10)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i + '0');
-		i++;
+	_putchar(c);
 	}
 	_putchar('\n');
 }
