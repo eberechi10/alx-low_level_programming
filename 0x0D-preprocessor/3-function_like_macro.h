1b698e36a3a45baabd8b164276b8fdb2551041b1
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-03-20 04:53:17 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef THREE_FUNCTION_LIKE_MACRO_H
-#define THREE_FUNCTION_LIKE_MACRO_H
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
 
-#endif 
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+
