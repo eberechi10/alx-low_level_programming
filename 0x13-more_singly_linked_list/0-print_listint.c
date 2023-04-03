7cdf280d5711efe290d5fb9f058109fb70e8191e
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-04-03 15:00:57 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_list/0-print_listint.c
+++ 0x13-more_singly_linked_list/0-print_listint.c
@@ -11,13 +11,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t sum = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		sum++;
+		count += 1;
 	}
-	return (sum);
+
+	return (count);
 }
