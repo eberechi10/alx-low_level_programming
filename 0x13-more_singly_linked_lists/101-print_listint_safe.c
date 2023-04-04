e5d54f77635d7ea350694094e6004b9e492ef0e2
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-04-04 03:37:52 -0700

    function can print lists with a loop

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - fuction to print a linked list.
+ *
+ * @head: is the head of list
+ * 
+ * go through the list only once.
+ * Return: number of nodes in list. exit with status 98 if failed.
+ *
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *get, *new;
+	size_t sum;
+
+	new = head;
+	if (new == NULL)
+		exit(98);
+
+
+	sum = 0;
+	while (new != NULL)
+	{
+		get = new;
+		new = new->next;
+		sum++;
+		printf("[%p] %d\n", (void *)get, get->n);
+
+
+
+		if (get < new)
+		{
+			printf("-> [%p] %d\n", (void *)new, new->n);
+			break;
+		}
+	}
+
+
+	return (sum);
+}
