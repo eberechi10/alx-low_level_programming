c575ee5c61656510787a87a34b08cd6578c10c94
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-04-03 20:17:03 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - the sum of the data (n) of a linked list.
+ * @head: it is the pointer to the node
+ *
+ * function that returns the sum of all the data (n) of a linked list.
+ *
+ * Return: sum all the data
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listrint_t *get = head;
+
+
+	while (get != NULL)
+	{
+		sum += get->n;
+		get = get->next;
+	}
+
+	return (sum);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - insert a node at postion
+ *
+ * @head: it is the pointer to the node
+ * @idx: the index
+ *
+ * @n: the new node to insert>
+ *
+ * Return: the address of new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int m;
+	listint_t *get, *new;
+
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
+	{
+		sun = *head;
+		for (m = 0; m < idx - 1 && get != NULL; m++)
+		{
+			get = get->next;
+		}
+		if (get == NULL)
+			return (NULL);
+	}
+	sun = malloc(sizeof(listint_t));
+	if (sun == NULL)
+		return (NULL);
+	sun->n = n;
+	if (idx == 0)
+	{
+		sun->next = *head;
+		*head = sun;
+		return (sun);
+	}
+	sun->next = sun->next;
+	get->next = sun;
+	return (sun);
+}
