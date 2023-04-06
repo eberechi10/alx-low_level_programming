567656433b8de1ebeefdb2bcf510d7e94d216ad9
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-04-06 14:50:30 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -40,7 +40,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	sun = get->next;
 	get->next = sun->next;
 	free(sun);
-
 	return (1);
-/*really getting better with time*/
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
