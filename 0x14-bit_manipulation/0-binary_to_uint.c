f812237d5d00388745ed726dd42b91fbfeb59c3f
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-04-08 01:29:29 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,27 +12,20 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int count;
 	int m;
-
-
+	
 	count = 0;
 	if (b == null)
 		return (0);
-
-
 	for (m = 0; b[m] != '\0'; m++)
 	{
 		if (b[m] != '0' && b[m] != '1')
 			return (0);
 	}
-
-
 	for (m = 0; b[m] != '\0'; m++)
 	{
-
 		count <<= 1;
 		if (b[m] == '1')
 			count += 1;
 	}
-
 	return (count);
 }
