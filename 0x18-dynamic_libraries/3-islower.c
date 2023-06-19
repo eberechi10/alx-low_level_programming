7f6986cb6e082e40f869389a1deef84b32c9020c
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-06-19 06:19:00 -0700

    dynamic library libdynamic.so

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - check if a character is lowercase.
+ *@c: the character to be checkef
+ * Return: 1 if its lowercas, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
