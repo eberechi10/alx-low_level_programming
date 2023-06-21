f8d8971c70dc9429662220caa3220860e3e34cc8
eberechi10 <eberechukwunemeremjohnsunday@gmail.com>
2023-06-21 01:50:47 -0700

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/eberechi10/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=/tmp/jackpot.so
+wget -P ../ https://github.com/eberechi10/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libjackpot.so
+export LD_PRELOAD=../libjackpot.so
